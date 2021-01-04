e773ab1bf2e87cf799094c3d5bd972973f63c797
Regis Diogo de Siqueira Filho <regis.siqueira@booking.com>
2021-01-04 15:15:36 +0100

    Removing unused express configs

--- index.js
+++ index.js
@@ -31,9 +31,6 @@ const handleChangeUser = (req, res) => {
 };
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  //.set('views', path.join(__dirname, 'views'))
-  //.set('view engine', 'ejs')
   .use(express.urlencoded({
     extended: true
   }))
