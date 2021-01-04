aef0c58b17915efb2a6e66f6e06ff97365bae1cc
Regis Diogo de Siqueira Filho <regis.siqueira@booking.com>
2021-01-04 14:03:09 +0100

    Creating a module to generate OneTrust script tags

---
+++ cookieBanner.js
@@ -0,0 +1,42 @@
+const jwt = require('jsonwebtoken');
+const {
+  v4: uuidv4
+} = require('uuid');
+const PUBLIC_KEY = 'Q29va2llIEJhbm5lciB1c2luZyBPbmVUcnVzdA==';
+
+const renderCrossConsentCookieBanner = function (uuid) {
+  return `<script src="https://cdn.cookielaw.org/consent/${uuid}/otSDKStub.js" 
+    type="text/javascript" charset="UTF-8" data-domain-script="${uuid}"
+    data-document-language="true"></script>
+  <script type="text/javascript">
+  function OptanonWrapper() { }
+  </script>`;
+};
+
+const getUserCrossConsentObject = function (userId) {
+  const token = jwt.sign({
+    sub: userId
+  }, PUBLIC_KEY, {
+    noTimestamp: true
+  });
+  console.log(userId);
+  return `<script type="text/javascript">
+  var OneTrust = {
+    dataSubjectParams: {
+      id: '${userId}',
+      token : '${token}'
+    }
+  };
+  </script>`;
+}
+
+exports.cookieBanner = function (uuid, userId) {
+  return `${getUserCrossConsentObject(userId)}
+  ${renderCrossConsentCookieBanner(uuid)}`;
+}
+
+exports.getUser = function () {
+  return {
+    id: uuidv4(),
+  };
+}
--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const express = require('express')
 const path = require('path');
+const {
+  cookieBanner,
+  getUser
+} = require('./cookieBanner');
 const PORT = process.env.PORT || 5000;
+
+const user = getUser();
 
 const renderIndexPage = (req, res) => {
   res.set('Content-Type', 'text/html');
   res.send(`<!DOCTYPE html>
   <html lang="en">
   <head>
+    ${cookieBanner('7d07b58a-5f7f-44bc-9e9f-d24d0d218497', user.id)}
   </head>
   <body>
     <h3>Hello test-one.booking.com!</h3>
     <form action="/user" method="POST">
-      Visitor id: <input type="text" name="user" style="width: 300px;">
+      Visitor id: <input type="text" name="user" value="${user.id}" style="width: 300px;">
       <input type="submit" value="Ok">
     </form>
   </body>
 </html>`);
 };
 
+const handleChangeUser = (req, res) => {
+  user.id = req.body.user;
+  res.redirect('/');
+};
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   //.set('views', path.join(__dirname, 'views'))
   //.set('view engine', 'ejs')
+  .use(express.urlencoded({
+    extended: true
+  }))
   .get('/', renderIndexPage)
+  .post('/user', handleChangeUser)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
